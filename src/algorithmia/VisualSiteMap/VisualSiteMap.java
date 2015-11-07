665b4e5b853f6f0334f764167644082b40e9480f
diego <diego@algorithmia.com>
2015-11-07 01:47:06 +0000

    Algorithmia web submission

--- src/algorithmia/VisualSiteMap/VisualSiteMap.java
+++ src/algorithmia/VisualSiteMap/VisualSiteMap.java
@@ -32,7 +32,7 @@ public class VisualSiteMap {
             URL url = new URL(key);
             String dataUrl = "data://.algo/perm/" + url.getHost() + "|" + url.getPath().replace("/","-") + ".png";
             System.out.println(dataUrl);
-            Object[] algoInput = {key, dataUrl, 1170, 0};
+            Object[] algoInput = {key, dataUrl, 0, 1170};
             String ok = Algorithmia.algo("bkyan/url2png").pipe(algoInput).as(new TypeToken<String>(){});
         }
         return null;
